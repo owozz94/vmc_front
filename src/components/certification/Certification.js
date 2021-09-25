18cc1aa7a3080bcc7b4321fb65dc0703c3a4cc7b
Jin young <yjy7643878@google.com>
2021-09-25 15:13:45 +0900

    Certifications form demo

--- src/components/certification/Certification.js
+++ src/components/certification/Certification.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
@@ -16,19 +16,13 @@ import TableRow from "@mui/material/TableRow";
 import TableSortLabel from "@mui/material/TableSortLabel";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Switch from "@mui/material/Switch";
-import DeleteIcon from "@mui/icons-material/Delete";
-import FilterListIcon from "@mui/icons-material/FilterList";
 import { visuallyHidden } from "@mui/utils";
-import { DataGrid } from "@mui/x-data-grid";
+import { format as dateFormat } from "date-fns";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DateRangePicker from "@mui/lab/DateRangePicker";
 import Button from "@restart/ui/esm/Button";
+import { BootstrapButton } from "../layout/Styles";
 
 function createData(id, date, name, market, coin, volume, rateOfReturn, amount) {
   return {
@@ -43,16 +37,18 @@ function createData(id, date, name, market, coin, volume, rateOfReturn, amount) 
   };
 }
 //임시데이터
-const rows = [
-  createData(0, "16 Mar, 2019", "Elvis Presley", "Bithumb", "BTC", 0.25, 15, 312044),
-  createData(1, "16 Mar, 2019", "Paul McCartney", "Bithumb", "UMA", 3, 12, 8660099),
-  createData(2, "16 Mar, 2019", "Tom Scholz", "Bithumb", "ETH", 4.25, 13, 100081),
-  createData(3, "16 Mar, 2019", "Michael Jackson", "Bithumb", "BTC", 2.0, 5, 654039),
-  createData(4, "15 Mar, 2019", "Bruce Springsteen", "Bithumb", "LUNA", 5.9, 4, 2120079),
-  createData(5, "15 Mar, 2019", "Bruce Springsteen", "Bithumb", "LUNA", 5.9, 4, 2120079),
-  createData(6, "15 Mar, 2019", "Bruce Springsteen", "Bithumb", "LUNA", 5.9, 4, 2120079),
-  createData(7, "15 Mar, 2019", "Bruce Springsteen", "Bithumb", "LUNA", 5.9, 4, 2120079),
+let rows = [
+  // createData(0, "16 Mar, 2019", "Elvis Presley", "Bithumb", "BTC", 0.25, 15, 312044),
+  // createData(1, "16 Mar, 2019", "Paul McCartney", "Bithumb", "UMA", 3, 12, 8660099),
+  // createData(2, "16 Mar, 2019", "Tom Scholz", "Bithumb", "ETH", 4.25, 13, 100081),
+  // createData(3, "16 Mar, 2019", "Michael Jackson", "Bithumb", "BTC", 2.0, 5, 654039),
+  // createData(4, "15 Mar, 2019", "Bruce Springsteen", "Bithumb", "LUNA", 5.9, 4, 2120079),
+  // createData(5, "15 Mar, 2019", "Bruce Springsteen", "Bithumb", "LUNA", 5.9, 4, 2120079),
+  // createData(6, "15 Mar, 2019", "Bruce Springsteen", "Bithumb", "LUNA", 5.9, 4, 2120079),
+  // createData(7, "15 Mar, 2019", "Bruce Springsteen", "Bithumb", "LUNA", 5.9, 4, 2120079),
+  { id: 0, date: "16 Mar, 2019", name: "Elvis Presley", market: "Bithumb", coin: "BTC", volume: 0.25, rateOfReturn: 15, amount: 312044 },
 ];
+
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
     return -1;
@@ -112,6 +108,18 @@ const headCells = [
     disablePadding: false,
     label: "volume",
   },
+  {
+    id: "rateOfReturn",
+    numeric: true,
+    disablePadding: false,
+    label: "rateOfReturn",
+  },
+  {
+    id: "amount",
+    numeric: true,
+    disablePadding: false,
+    label: "amount",
+  },
 ];
 function EnhancedTableHead(props) {
   const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
@@ -122,24 +130,8 @@ function EnhancedTableHead(props) {
   return (
     <TableHead>
       <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              "aria-label": "select all desserts",
-            }}
-          />
-        </TableCell>
         {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? "right" : "left"}
-            padding={headCell.disablePadding ? "none" : "normal"}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
+          <TableCell key={headCell.id} align={"center"} padding={headCell.disablePadding ? "none" : "normal"} sortDirection={orderBy === headCell.id ? order : false}>
             <TableSortLabel active={orderBy === headCell.id} direction={orderBy === headCell.id ? order : "asc"} onClick={createSortHandler(headCell.id)}>
               {headCell.label}
               {orderBy === headCell.id ? (
@@ -183,22 +175,8 @@ const EnhancedTableToolbar = (props) => {
         </Typography>
       ) : (
         <Typography sx={{ flex: "1 1 100%" }} variant="h6" id="tableTitle" component="div">
-          Nutrition
+          조회목록
         </Typography>
-      )}
-
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton>
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton>
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
       )}
     </Toolbar>
   );
@@ -219,6 +197,26 @@ export default function Certification() {
   const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
+  // const init = () => {
+  //   setValue(["2021-08-31T15:00:00.000Z", null]);
+  // };
+  // init();
+  useEffect(() => {
+    setValue([new Date() - 300000, new Date()]);
+  }, []);
+
+  function handleSearch() {
+    //axios 조회 todo
+
+    rows = [
+      { id: 0, date: "17 Mar, 2019", name: "Elvis Presley", market: "Bithumb", coin: "BTC", volume: 0.25, rateOfReturn: 15, amount: 312044 },
+      { id: 1, date: "18 Mar, 2019", name: "Elvis Presley", market: "Bithumb", coin: "BTC", volume: 0.25, rateOfReturn: 15, amount: 312044 },
+      { id: 2, date: "19 Mar, 2019", name: "Elvis Presley", market: "Bithumb", coin: "BTC", volume: 0.25, rateOfReturn: 15, amount: 312044 },
+      { id: 3, date: "20 Mar, 2019", name: "Elvis Presley", market: "Bithumb", coin: "BTC", volume: 0.25, rateOfReturn: 15, amount: 312044 },
+    ]; //가져온 데이터
+    //setPage(0);
+    setRowsPerPage(10);
+  }
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === "asc";
     setOrder(isAsc ? "desc" : "asc");
@@ -273,10 +271,11 @@ export default function Certification() {
     //Container 시작
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
       <Box sx={{ width: "100%" }}>
-        <Paper sx={{ width: "100%", mb: 2 }}>
+        <Paper sx={{ width: "100%", mb: 2, pl: 5, pr: 5, pt: 3 }}>
+          <h3>인증서 발급하기</h3>
           {/* 
       /**input 시작 */}
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Container maxWidth="lg" sx={{ mt: 3, mb: 4, ml: -4 }}>
             <Grid item xs={12}>
               <Box
                 component="form"
@@ -286,9 +285,21 @@ export default function Certification() {
                 noValidate
                 autoComplete="off"
               >
-                <TextField id="outlined-password-input" label="Public Key" type="Private Key" autoComplete="current-password" />
+                <TextField
+                  sx={{ color: "text.primary", fontSize: 28, fontWeight: "medium" }}
+                  id="outlined-password-input"
+                  label="Public Key"
+                  type="Public Key"
+                  autoComplete="current-password"
+                />
                 <div>
-                  <TextField id="outlined-password-input" label="Private Key" type="Private Key" autoComplete="current-password" />
+                  <TextField
+                    sx={{ color: "text.primary", fontSize: 28, fontWeight: "medium" }}
+                    id="outlined-password-input"
+                    label="Private Key"
+                    type="Private Key"
+                    autoComplete="current-password"
+                  />
                 </div>
               </Box>
             </Grid>
@@ -303,7 +314,8 @@ export default function Certification() {
           </Container>
           <LocalizationProvider dateAdapter={AdapterDateFns}>
             <div>
-              <Typography sx={{ mt: 2, mb: 1 }}>1 calendar </Typography>
+              <Typography sx={{ mt: 2, mb: 2 }}>조회하기</Typography>
+
               <DateRangePicker
                 calendars={1}
                 value={value}
@@ -317,16 +329,18 @@ export default function Certification() {
                     <TextField {...endProps} />
                   </React.Fragment>
                 )}
+                //inputFormat="yyyyMMdd"
               />
             </div>
           </LocalizationProvider>
-
-          <Button variant="contained">조회</Button>
+          <BootstrapButton variant="contained" onClick={handleSearch}>
+            조회
+          </BootstrapButton>
           <Button variant="contained">발행</Button>
-
+          {/* 테이블 시작 */}
           <EnhancedTableToolbar numSelected={selected.length} />
           <TableContainer>
-            <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle" size={dense ? "small" : "medium"}>
+            <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle" aria-label="sticky table" size={dense ? "small" : "medium"}>
               <EnhancedTableHead
                 numSelected={selected.length}
                 order={order}
@@ -351,25 +365,18 @@ export default function Certification() {
                         role="checkbox"
                         aria-checked={isItemSelected}
                         tabIndex={-1}
-                        key={row.name}
+                        key={row.id}
                         selected={isItemSelected}
                       >
-                        <TableCell padding="checkbox">
-                          <Checkbox
-                            color="primary"
-                            checked={isItemSelected}
-                            inputProps={{
-                              "aria-labelledby": labelId,
-                            }}
-                          />
-                        </TableCell>
                         <TableCell component="th" id={labelId} scope="row" padding="none">
-                          {row.name}
+                          {row.date}
                         </TableCell>
-                        <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
+                        <TableCell align="right">{row.name}</TableCell>
+                        <TableCell align="right">{row.market}</TableCell>
+                        <TableCell align="right">{row.coin}</TableCell>
+                        <TableCell align="right">{row.volume}</TableCell>
+                        <TableCell align="right">{row.rateOfReturn}</TableCell>
+                        <TableCell align="right">{row.amount}</TableCell>
                       </TableRow>
                     );
                   })}
