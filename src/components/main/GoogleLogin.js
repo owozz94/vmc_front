269212a44d434b870d71aef5555d205194988b16
Jin young <yjy7643878@google.com>
2021-10-03 00:45:02 +0900

    구글버튼으로 수정

--- src/components/main/GoogleLogin.js
+++ src/components/main/GoogleLogin.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import styles from '../../css/GoogleLogin.module.css'
-import {Button} from 'react-bootstrap'
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import styles from "../../css/GoogleLogin.module.css";
+import GoogleButton from "./GoogleButton";
 export default class GoogleLogin extends Component {
-    static propTypes = {
-        prop: PropTypes
-    }
+  static propTypes = {
+    prop: PropTypes,
+  };
 
-    render() {
-        return (
-            <div>
-                <Button variant="primary" className={styles.googleBtn}>google로 로그인</Button>
-            </div>
-        )
-    } 
+  render() {
+    return (
+      <div>
+        <GoogleButton variant="primary" className={styles.googleBtn}>
+          google로 로그인
+        </GoogleButton>
+      </div>
+    );
+  }
 }
