80c7065317159cb4e06560c62ae7e277ace68218
sophysophysophysophy <61459@naver.com>
2021-10-05 21:28:14 +0900

    change meta tag's location

--- src/components/main/GoogleLogin.js
+++ src/components/main/GoogleLogin.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import styles from "../../css/GoogleLogin.module.css";
 import GoogleButton from "./GoogleButton";
-import MetaTags from 'react-meta-tags';
 export default class GoogleLogin extends Component {
   static propTypes = {
     prop: PropTypes,
@@ -11,14 +10,9 @@ export default class GoogleLogin extends Component {
   render() {
     return (
       <div>
-         <MetaTags>
-         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
-        </MetaTags>
-      <div>
         <GoogleButton variant="primary" className={styles.googleBtn}>
           google로 로그인
         </GoogleButton>
-      </div>
       </div>
     );
   }
