aeb629e22ae3ac9372690584d7eda10445995003
Jin young <yjy7643878@google.com>
2021-10-06 17:45:46 +0900

    proxy 삭제

--- src/proxy/setupProxy.js
+++ src/proxy/setupProxy.js
@@ -1,11 +1,3 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 //api로  시작되는 호출은 target에 적힌 위치로 호출되도록 설정
-module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "https://3.37.123.157:8000", //서버 포트
-      changeOrigin: true,
-    })
-  );
-};
+module.exports = function (app) {};
