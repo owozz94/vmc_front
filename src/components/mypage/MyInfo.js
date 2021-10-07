8d48153b4615b453510891061dbdc290530a8529
taehoonhwang <on91716@gmail.com>
2021-10-07 22:05:35 +0900

    feat: MyPage 기본정보 데이터 렌더링 완료

--- src/components/mypage/MyInfo.js
+++ src/components/mypage/MyInfo.js
@@ -1,74 +1,52 @@
 // eslint-disable-next-line
-import React from 'react'
-// import PropTypes from 'prop-types'
+import React, {useEffect, useState} from 'react'
 import {Col, Row} from 'react-bootstrap'
 import styles from '../../css/MyInfo.module.css'
-import axios from 'axios';
+import ComAxios from "./../../util/ComAxios";
 
 export default function MyInfo () {
+    useEffect(() => {
+        initCoin();
+    }, []);
 
-    axios.get("http://3.37.123.157:8000/user/userInfo")
-    .then((Response)=>{console.log('성공')})
-    // .then((Response)=>{console.log(Response.data)})
-    // .catch((Error)=>{console.log(Error)})
-    .catch((Error)=>{console.log('에러')})
+    const [userInfo, setUserInfo] = useState({});
 
-        return (
-            <div className={styles.InfoTable}>
-                <article className={styles.InfoCategory}>
-                    <h2 className={styles.title}>기본 정보</h2>
-                    <Row className={styles.InfoRow}>
-                        <Col xs={6} md={4}>사진</Col>
-                        <Col xs={6} md={8}>이미지</Col>
-                    </Row>
-                    <Row className={styles.InfoRow}>
-                        <Col xs={6} md={4}>이름</Col>
-                        <Col xs={6} md={8}>김빗썸</Col>
-                    </Row>
-                    <Row className={styles.InfoRow}>
-                        <Col xs={6} md={4}>닉네임</Col>
-                        <Col xs={6} md={8}>username</Col>
-                    </Row>
-                    <Row className={styles.InfoRow}>
-                        <Col xs={6} md={4}>가입날짜</Col>
-                        <Col xs={6} md={8}>2020. 01. 01.</Col>
-                    </Row>
-                </article>
-                <article className={styles.InfoCategory}>
-                    <h2 className={styles.title}>연락처 정보</h2>
-                    <Row className={styles.InfoRow}>
-                        <Col xs={6} md={4}>이메일</Col>
-                        <Col xs={6} md={8}>bithumb@gmail.com</Col>
-                    </Row>
-                    <Row className={styles.InfoRow}>
-                        <Col xs={6} md={4}>휴대전화</Col>
-                        <Col xs={6} md={8}>010-1234-5678</Col>
-                    </Row>
-                </article>
-                {/* <Table striped bordered hover> */}
-                {/* <Table bordered>
-                    <thead>
-                        <tr>
-                        <th></th>
-                        <th>회원정보</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <td>이름</td>
-                        <td colSpan="4">김빗썸</td>
-                        </tr>
-                        <tr>
-                        <td>이메일</td>
-                        <td colSpan="4">bithumb@gmail.com</td>
-                        </tr>
-                        <tr>
-                        <td >가입날짜</td>
-                        <td colSpan="4">2021. 02. 20.</td>
-                        </tr>
-                    </tbody>
-                </Table> */}
+    const initCoin = () => {
+        ComAxios({
+            method: "get",
+            url: "http://3.37.123.157:8000/user/info",
+        })
+            .then((res) => {
+                setUserInfo(res.data.data)
+            })
+            .catch((err) => {
+                console.log(`err : ${err}`);
+            });
+    };
 
-            </div>
-        )
+    return (
+        <div className={styles.InfoTable}>
+            <article className={styles.InfoCategory}>
+                <h2 className={styles.title}>기본 정보</h2>
+                <Row className={styles.InfoRow}>
+                    <Col xs={6} md={4}>사진</Col>
+                    <Col xs={6} md={8}>
+                        <img className="userImage" alt="User Image" src={userInfo.picture} />
+                    </Col>
+                </Row>
+                <Row className={styles.InfoRow}>
+                    <Col xs={6} md={4}>이름</Col>
+                    <Col xs={6} md={8}>{userInfo.family_name} {userInfo.given_name}</Col>
+                </Row>
+                <Row className={styles.InfoRow}>
+                    <Col xs={6} md={4}>닉네임</Col>
+                    <Col xs={6} md={8}>{userInfo.name}</Col>
+                </Row>
+                <Row className={styles.InfoRow}>
+                    <Col xs={6} md={4}>이메일</Col>
+                    <Col xs={6} md={8}>{userInfo.email}</Col>
+                </Row>
+            </article>
+        </div>
+    )
 }
