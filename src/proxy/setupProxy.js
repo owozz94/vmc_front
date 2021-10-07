fa878b13ef22ab1b05ad1f6cdf2e415640de045e
hyemyn2 <hyemyn2@gmail.com>
2021-10-07 15:57:39 +0900

    my certification api 호출

--- src/proxy/setupProxy.js
+++ src/proxy/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 //api로  시작되는 호출은 target에 적힌 위치로 호출되도록 설정
-module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "https://3.37.123.157:8000", //서버 포트
-      changeOrigin: true,
-    })
-  );
+module.exports = function(app) {
+    app.use(
+        "/api",
+        createProxyMiddleware({
+            target: "http://3.37.123.157:8000", //서버 포트
+            changeOrigin: true,
+        })
+    );
 };
